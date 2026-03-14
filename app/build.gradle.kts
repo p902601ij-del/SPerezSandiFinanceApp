ddbc2593399d236c26e863aa690322657fcbb2f5
p902601ij-del <p902601ij@gmail.com>
2026-03-14 16:51:22 -0600

    se arreglo problemas de renderizado con gradle con ayuda de la IA

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,6 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.material3)
     
     // Iconos extendidos para que AccountCircle funcione
     implementation(libs.androidx.compose.material.icons.extended)
