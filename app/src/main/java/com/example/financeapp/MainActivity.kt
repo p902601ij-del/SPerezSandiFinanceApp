44ba4eb16e6d9f7a61374a95d5d4de389d2d818a
p902601ij-del <p902601ij@gmail.com>
2026-03-14 10:11:41 -0600

    Base del proyecto

--- app/src/main/java/com/example/financeapp/MainActivity.kt
+++ app/src/main/java/com/example/financeapp/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.financeapp
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.financeapp.ui.theme.FinanceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             FinanceAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    FinanceApp(innerPadding)
                 }
             }
         }
@@ -31,17 +38,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FinanceApp(innerPadding: PaddingValues) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+    ){
+        //header
+        Row() {
+            Box(
+
+            ) { }
+            Column(
+
+            ) { }
+            Icon(
+                imageVector = "",
+                contentDescription = null,
+                color = Color.Black
+            )
+        }
+        // Summary Cards
+        Row() {
+            //aqui va la primera cards
+            Column() {
+                //aqui van las siguientes dos cards
+            }
+        }
+        Row() {
+            Text(
+                text = "Transactions"
+            )
+            Text(
+                text = "See All"
+            )
+        }
+        LazyColumn() { }
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     FinanceAppTheme {
-        Greeting("Android")
+        FinanceApp(innerPadding = PaddingValues(top = 15.dp))
     }
 }
