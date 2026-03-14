767f6064f621ec0be3b8ae9dcdd867e8f9e9fed3
p902601ij-del <p902601ij@gmail.com>
2026-03-14 17:14:46 -0600

    implementacion final de los componentes en MainActivity

--- app/src/main/java/com/example/financeapp/MainActivity.kt
+++ app/src/main/java/com/example/financeapp/MainActivity.kt
@@ -5,36 +5,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.financeapp.components.HeaderComponent
 import com.example.financeapp.components.SummaryCardComponent
-import com.example.financeapp.models.User
-import com.example.financeapp.ui.theme.* 
+import com.example.financeapp.components.TransactionsComponent
+import com.example.financeapp.ui.theme.AppBackground
 import com.example.financeapp.ui.theme.FinanceAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -58,22 +41,11 @@ fun FinanceApp(innerPadding: PaddingValues) {
             .fillMaxSize()
             .background(AppBackground)
             .padding(innerPadding)
-    ){
+    ) {
         HeaderComponent()
         SummaryCardComponent()
-        //Transactions
-        Row() {
-            Text(
-                text = "Transactions"
-            )
-            Text(
-                text = "See All"
-            )
-        }
-        //
-        LazyColumn() { }
+        TransactionsComponent(modifier = Modifier.weight(1f))
     }
-
 }
 
 @Preview(showBackground = true)
